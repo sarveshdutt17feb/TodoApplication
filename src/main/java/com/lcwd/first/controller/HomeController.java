60bda410aeb423f4bafc908814827a38bdd9c926
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-05 16:48:59 +0530

    Spring Boot config reading data from application.properties file @Value(${}) annotation

--- src/main/java/com/lcwd/first/controller/HomeController.java
+++ src/main/java/com/lcwd/first/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.lcwd.first.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,17 @@ import java.util.List;
 
 @RestController
 public class HomeController {
-   @RequestMapping("/todos")
+
+    @Value("${lcwd.profile.img.path}")
+    private String profilePath;
+    @RequestMapping("/todos")
     public List<String> justTest(){
         List<String> todos = Arrays.asList("Learn Java", "Learn SpringBoot", "Develop Project","Learn Nodejs","Learn Oops");
         return todos;
     }
+    @RequestMapping("/profilePath")
+    public String getProfilePath(){
+        return profilePath;
+    }
+
 }
